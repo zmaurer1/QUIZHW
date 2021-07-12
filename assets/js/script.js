bf362f63169a5831a51aa25dabd0724ffa20af52
Zach Maurer <zmaurer88@yahoo.com>
2021-07-12 16:13:15 -0700

    quiz update

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var timerDiv = document.querySelector("#time")
 var choicesDiv = document.querySelector("#Choices")
 var startBtn = document.querySelector(".start-button")
 var submitButton = document.querySelector("#submit")
-var initials = document.querySelector("#initials")
+var initialsDiv = document.querySelector("#initials")
 var resetButton = document.querySelector(".reset-button")
 
 function startquiz(){
@@ -29,13 +29,38 @@ function getQuestion(){
         choiceButton.setAttribute("class", "choice")
         choiceButton.setAttribute("value", choice)
         choiceButton.textContent = i+choice
+        choiceButton.onclick=answerClick
         choicesDiv.appendChild(choiceButton)
-
     
     })
 
 }
+function answerClick(){
+if(this.value !==questions[questionIndex].answer){
+    time-=15
+    if(time<0){
+        time=0
+    }
+    timerDiv.textContent=time
+    
+}
+console.log("clicked")
+
+questionIndex++
+if(questionIndex===questions.length){
+    quizover()
+}else{
+    getQuestion()
+}
+
+}
 function quizover(){
+    clearInterval(timerId)
+    var endScreenDiv=document.getElementById("endscreen")
+    endScreenDiv.removeAttribute("class")
+    var finalScore=document.getElementById("final-score")
+    finalScore.textContent=time
+    questionsDiv.setAttribute("class", "hide")
 
 }
 function clocktick(){
@@ -45,5 +70,34 @@ function clocktick(){
         quizover()
     }
 }
+function saveHighScore(){
+    var initials=initialsDiv.value.trim()
+    if(initials!==""){
+        var highScores=JSON.parse(window.localStorage.getItem("highScores"))||[]
+        var newScore={
+            score:time,
+            initials:initials
+        }
+        highScores.push(newScore)
+        window.localStorage.setItem("highScores", JSON.stringify(highScores))
+
+    }
+
+
+}
+function getHighScores(){
+    var highScores=JSON.parse(window.localStorage.getItem("highScores"))||[]
+    highScores.sort(function(a,b){
+        return b.score -a.score
+    })
+    highScores.forEach(function(score){
+        var listTag=document.createElement("li")
+        listTag.textContent=score.initials+"-"+score.score
+        var olContainer=document.getElementById("highScoresList")
+        olContainer.appendChild(listTag)
+    })
+}
 resetButton.addEventListener("click", window.location.reload)
 startBtn.addEventListener("click", startquiz)
+submitButton.onclick=saveHighScore
+getHighScores()
